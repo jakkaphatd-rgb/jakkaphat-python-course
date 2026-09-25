ce9f8e8528b69e4e443c8a76cc61035e3cea706d
jakkaphatd-rgb <jakkaphat.d@ku.th>
2026-09-25 11:59:11 +0700

    quizw7

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -5,6 +5,13 @@
     - มี method ชื่อ get_perimeter() ที่คืนค่ารอบรูปของสี่เหลี่ยม
 """
 
+"""
+    สร้าง class Rectangle โดยกำหนดให้
+    - มี attribute ชื่อ length และ width ที่เก็บข้อมูลความยาวและความกว้างของสี่เหลี่ยม
+    - มี method ชื่อ get_area() ที่คืนค่าพื้นที่ของสี่เหลี่ยม
+    - มี method ชื่อ get_perimeter() ที่คืนค่ารอบรูปของสี่เหลี่ยม
+"""
+
 class Rectangle:
     def __init__(self, length, width):
         self.length = length
@@ -12,13 +19,38 @@ class Rectangle:
 
     # Method to get the area
     def get_area(self):
-        pass
+        return self.length * self.width
 
     # Method to get the perimeter
     def get_perimeter(self):
-        pass
+        return 2 * (self.length + self.width)
 
 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+
+
+""" 
+ขอให้เขียนคลาส circle ที่ทำงานคล้ายคลึงกับคลาส rectangle พร้อมตัวอย่าง
+"""
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    # Method to get the area
+    def get_area(self):
+        return 3.1416 * self.radius * self.radius
+
+    # Method to get the perimeter
+    def get_perimeter(self):
+        return 2 * 3.1416 * self.radius
+
+
+circle = Circle(10)
+print(circle.get_area())       # Should print 314.16
+print(circle.get_perimeter())  # Should print 62.832
+ 
+
--- labs/week07-lab/example/Student.py
+++ labs/week07-lab/example/Student.py
@@ -32,3 +32,6 @@ print(student1.introduce())
 print(student1.add_course("Python Programming"))
 print(student1.add_course("Data Structures"))
 print(student1.show_courses())
+
+print(student2.introduce())
+print(student2.)
